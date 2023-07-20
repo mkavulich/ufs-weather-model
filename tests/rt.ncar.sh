484837a5f67c926cad70de6b35f3cae46b2e8eaa
Michael Kavulich, Jr <kavulich@ucar.edu>
2023-07-20 23:34:08 +0000

    maybe this will work

--- tests/rt.ncar.sh
+++ tests/rt.ncar.sh
@@ -134,7 +134,7 @@ export delete_rundir=false
 
 TESTS_FILE='rt.conf'
 
-while getopts ":cl:mn:dwkreh" opt; do
+while getopts ":cl:mn:dw:krepsh" opt; do
   case $opt in
     c)
       CREATE_BASELINE=true
