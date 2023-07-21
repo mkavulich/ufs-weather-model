036ca667f450e8b5bd4a4eb5c1394c2f5c0f4b70
Michael Kavulich, Jr <kavulich@ucar.edu>
2023-07-21 02:40:28 +0000

    Finally figured out how to manually tests properly...this is close to working...for real this time!

--- tests/rt.ncar.sh
+++ tests/rt.ncar.sh
@@ -189,12 +189,12 @@ done
 
 # Default compiler "intel"
 export RT_COMPILER=${RT_COMPILER:-intel}
-
+MACHINE_ID=${MACHINE_ID}.${RT_COMPILER}
 source rt_utils.sh
 
 source module-setup.sh
 
-if [[ $MACHINE_ID = wcoss2 ]]; then
+if [[ $MACHINE_ID = wcoss2.* ]]; then
 
   #module use /usrx/local/dev/emc_rocoto/modulefiles
   #module load ruby/2.5.1 rocoto/1.3.0rc2
@@ -224,7 +224,7 @@ if [[ $MACHINE_ID = wcoss2 ]]; then
   cp fv3_conf/fv3_qsub.IN_wcoss2 fv3_conf/fv3_qsub.IN
   cp fv3_conf/compile_qsub.IN_wcoss2 fv3_conf/compile_qsub.IN
 
-elif [[ $MACHINE_ID = acorn ]]; then
+elif [[ $MACHINE_ID = acorn.* ]]; then
 
   module load ecflow/5.6.0.13
   module load gcc/10.3.0 python/3.8.6
@@ -247,7 +247,7 @@ elif [[ $MACHINE_ID = acorn ]]; then
   cp fv3_conf/fv3_qsub.IN_wcoss2 fv3_conf/fv3_qsub.IN
   cp fv3_conf/compile_qsub.IN_wcoss2 fv3_conf/compile_qsub.IN
 
-elif [[ $MACHINE_ID = gaea ]]; then
+elif [[ $MACHINE_ID = gaea.* ]]; then
 
   export PATH=/lustre/f2/pdata/esrl/gsd/contrib/miniconda3/4.8.3/envs/ufs-weather-model/bin:/lustre/f2/pdata/esrl/gsd/contrib/miniconda3/4.8.3/bin:$PATH
   export PYTHONPATH=/lustre/f2/pdata/esrl/gsd/contrib/miniconda3/4.8.3/envs/ufs-weather-model/lib/python3.8/site-packages:/lustre/f2/pdata/esrl/gsd/contrib/miniconda3/4.8.3/lib/python3.8/site-packages
@@ -266,7 +266,7 @@ elif [[ $MACHINE_ID = gaea ]]; then
   cp fv3_conf/fv3_slurm.IN_gaea fv3_conf/fv3_slurm.IN
   cp fv3_conf/compile_slurm.IN_gaea fv3_conf/compile_slurm.IN
 
-elif [[ $MACHINE_ID = hera ]]; then
+elif [[ $MACHINE_ID = hera.* ]]; then
 
   module load rocoto
   ROCOTORUN=$(which rocotorun)
@@ -295,7 +295,7 @@ elif [[ $MACHINE_ID = hera ]]; then
   cp fv3_conf/fv3_slurm.IN_hera fv3_conf/fv3_slurm.IN
   cp fv3_conf/compile_slurm.IN_hera fv3_conf/compile_slurm.IN
 
-elif [[ $MACHINE_ID = orion ]]; then
+elif [[ $MACHINE_ID = orion.* ]]; then
 
   module load gcc/8.3.0
 
@@ -320,7 +320,7 @@ elif [[ $MACHINE_ID = orion ]]; then
   cp fv3_conf/fv3_slurm.IN_orion fv3_conf/fv3_slurm.IN
   cp fv3_conf/compile_slurm.IN_orion fv3_conf/compile_slurm.IN
 
-elif [[ $MACHINE_ID = jet ]]; then
+elif [[ $MACHINE_ID = jet.* ]]; then
 
   module load rocoto/1.3.2
   ROCOTORUN=$(which rocotorun)
@@ -346,7 +346,7 @@ elif [[ $MACHINE_ID = jet ]]; then
   cp fv3_conf/fv3_slurm.IN_jet fv3_conf/fv3_slurm.IN
   cp fv3_conf/compile_slurm.IN_jet fv3_conf/compile_slurm.IN
 
-elif [[ $MACHINE_ID = s4 ]]; then
+elif [[ $MACHINE_ID = s4.* ]]; then
 
   module load rocoto/1.3.2
   module load ecflow/5.6.0
@@ -373,7 +373,7 @@ elif [[ $MACHINE_ID = s4 ]]; then
   cp fv3_conf/fv3_slurm.IN_s4 fv3_conf/fv3_slurm.IN
   cp fv3_conf/compile_slurm.IN_s4 fv3_conf/compile_slurm.IN
 
-elif [[ $MACHINE_ID = cheyenne ]]; then
+elif [[ $MACHINE_ID = cheyenne.* ]]; then
 
   export PATH=/glade/p/ral/jntp/tools/miniconda3/4.8.3/envs/ufs-weather-model/bin:/glade/p/ral/jntp/tools/miniconda3/4.8.3/bin:$PATH
   export PYTHONPATH=/glade/p/ral/jntp/tools/miniconda3/4.8.3/envs/ufs-weather-model/lib/python3.8/site-packages:/glade/p/ral/jntp/tools/miniconda3/4.8.3/lib/python3.8/site-packages
@@ -392,7 +392,7 @@ elif [[ $MACHINE_ID = cheyenne ]]; then
   cp fv3_conf/fv3_qsub.IN_cheyenne fv3_conf/fv3_qsub.IN
   cp fv3_conf/compile_qsub.IN_cheyenne fv3_conf/compile_qsub.IN
 
-elif [[ $MACHINE_ID = stampede ]]; then
+elif [[ $MACHINE_ID = stampede.* ]]; then
 
   export PYTHONPATH=
   ECFLOW_START=
@@ -409,7 +409,7 @@ elif [[ $MACHINE_ID = stampede ]]; then
   MPIEXECOPTS=
   cp fv3_conf/fv3_slurm.IN_stampede fv3_conf/fv3_slurm.IN
 
-elif [[ $MACHINE_ID = expanse ]]; then
+elif [[ $MACHINE_ID = expanse.* ]]; then
 
   export PYTHONPATH=
   ECFLOW_START=
